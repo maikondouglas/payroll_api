ae2187be7005db27c7104a182814ab9a1d32c87b
Maikon Douglas <maikonconta0@gmail.com>
2026-03-06 00:41:36 -0300

    remove commits

--- lib/payroll_api/accounts/user.ex
+++ lib/payroll_api/accounts/user.ex
@@ -4,8 +4,8 @@ defmodule PayrollApi.Accounts.User do
 
   schema "users" do
     field :name, :string
-    field :email, :string # <-- E-mail volta para cá
-    field :cpf, :string   # <-- CPF continua aqui
+    field :email, :string
+    field :cpf, :string
     field :role, :string, default: "employee"
     field :password, :string, virtual: true
     field :password_hash, :string
@@ -15,13 +15,13 @@ defmodule PayrollApi.Accounts.User do
 
   def changeset(user, attrs) do
     user
-    |> cast(attrs, [:name, :email, :cpf, :role, :password]) # Inclui os dois no cast
-    |> validate_required([:name, :email, :cpf, :password])  # Exige os dois
+    |> cast(attrs, [:name, :email, :cpf, :role, :password])
+    |> validate_required([:name, :email, :cpf, :password])
     |> validate_format(:email, ~r/^[^\s]+@[^\s]+$/, message: "formato inválido")
     |> validate_length(:cpf, is: 11, message: "deve conter exatamente 11 dígitos")
     |> validate_length(:password, min: 6)
-    |> unique_constraint(:email) # Garante e-mail único
-    |> unique_constraint(:cpf)   # Garante CPF único
+    |> unique_constraint(:email)
+    |> unique_constraint(:cpf)
     |> put_pass_hash()
   end
 
